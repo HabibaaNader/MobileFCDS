d6b57b241253c00f164101bb5f39336bb0526822
HabibaaNader <140197508+HabibaaNader@users.noreply.github.com>
2023-11-02 09:22:14 +0200

    edited

--- Activity2.java
+++ Activity2.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
+    private Button back_button,login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_main2);
+
+        back_button = (Button) findViewById(R.id.button2);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity1();
+            }
+        });
+
+        login_button = (Button) findViewById(R.id.button3);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+    }
+    public void openActivity1() {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+    }
+    public void openActivity3() {
+        Intent intent1 = new Intent(this, MainActivity3.class);
+        startActivity(intent1);
     }
 }
